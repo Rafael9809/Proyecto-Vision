424c90fbe284c80c2df11c5bc65989159e530edd
Gnrlml9809 <rafael98upiita@hotmail.com>
2023-06-13 15:33:01 -0600

    Menu agregado

--- detectando.py
+++ detectando.py
@@ -5,13 +5,23 @@ from threading import Timer
 def Pass():
     pass
 
+def Menu():
+    a=input("Introduzca modo de operacion\n1) Tomar foto y cerrar\n2) Introducir numero maximo de fotos antes de cerrar\n3) Funcionamiento continuo\n>")
+    if (a=="1"):
+        iniciar_detectando(1)
+    elif (a=="2"):
+        b=input("Introduzca el numero maximo de fotos\n>")
+        iniciar_detectando(int(b))
+    elif(a=="3"):
+        iniciar_detectando(0)
 
 
-def iniciar_detectando():
-    cap = cv2.VideoCapture(0)
+def iniciar_detectando(max):
+    cap = cv2.VideoCapture(1)
     i=0
     j=0
     b=False
+    c=False
     sonrisa = cv2.CascadeClassifier('haarcascade_smile.xml')
     flash= np.ones((300, 300, 3), np.uint8) * 255
     while True:
@@ -40,7 +50,13 @@ def iniciar_detectando():
             except:
                 print('Foto no almacenada')
                 i=0
+            if(j>0):
+                if(j==max):
+                    c=True
+                    break
         cv2.imshow('frame',frame)
+        if c:
+            break
         if b:
             t = Timer(2, Pass)  
             t.start()
@@ -51,4 +67,5 @@ def iniciar_detectando():
     cap.release()
     cv2.destroyAllWindows()
 
-iniciar_detectando()
+Menu()
+#iniciar_detectando(0)
